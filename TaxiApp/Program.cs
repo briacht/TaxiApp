1516b9ae94c82a905fca86b71c04ca74a04f9bbd
Bri Achtman <brachtma@microsoft.com>
2021-05-05 10:39:35 -0700

    add sentiment model

--- TaxiApp/Program.cs
+++ TaxiApp/Program.cs
@@ -6,7 +6,25 @@ namespace TaxiApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Sentiment Analysis:
+            Console.WriteLine("Write a review of your taxi driver: ");
+
+            //Load input data
+            var sampleData = new ReviewSentimentModel.ModelInput()
+            {
+                Comment = Console.ReadLine()
+            };
+
+            //Load model and predict output
+            var result = ReviewSentimentModel.Predict(sampleData);
+
+            string printedString = "";
+
+            if (result.Prediction == "positive") { printedString = "Thanks for the great review! :)"; };
+
+            if (result.Prediction == "negative") { printedString = "We're sorry you had a bad experience. :("; };
+
+            Console.WriteLine($"\n{printedString}");
         }
     }
 }
